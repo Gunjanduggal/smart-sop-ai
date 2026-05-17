0837ef20fc85fdf85da861de29aeed4a958060a4
gunjanduggal <duggal2706@gmail.com>
2026-05-17 11:19:26 +0530

    insights

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ require('./routes/productroutes');
 const inventoryRoutes =
 require('./routes/inventoryRoutes');
 
+const insightsRoutes =
+require('./routes/insightsRoutes');
+
 const app = express();
 
 connectDB();
@@ -25,6 +28,8 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 
 app.use('/api/inventory', inventoryRoutes);
+
+app.use('/api/insights', insightsRoutes);
 
 const PORT = process.env.PORT || 5000;
 
